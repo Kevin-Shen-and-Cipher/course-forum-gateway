b6e456a22283278dd7bc53b4355497214302ace4
Dragon <C109151163@nkust.edu.tw>
2023-04-17 14:24:02 +0800

    fix:Update API_URL style

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\App;
 
 class PostController extends Controller 
 {
@@ -12,42 +11,40 @@ class PostController extends Controller
 
     public function __construct()
     {
-        $this->API_URL=env('APP_COURSE_FORUM_API');
+        $this->API_URL = env('APP_COURSE_FORUM_API')."/posts" ;
     }
 
     public function index()
     {
-        // ddd("{$this->API_URL}/posts");
-        $response = Http::get("{$this->API_URL}/posts"); 
+        $response = Http::get($this->API_URL); 
         $posts = $response->json();
-
         return response()->json($posts);
     }
 
     public function store(Request $request)
     {
         $data = $request->all(); 
-        $response = Http::post("{$this->API_URL}/posts", $data); 
+        $response = Http::post($this->API_URL, $data); 
         return $response->json();
     }
 
     public function show(string $id)
     {
-        $response = Http::get("{$this->API_URL}/posts/{$id}"); 
+        $response = Http::get($this->API_URL."/{$id}"); 
         $posts = $response->json();
         return response()->json($posts);
     }
 
     public function update(Request $request, string $id)
     {
-        $response = Http::patch("{$this->API_URL}/posts/{$id}",$request); 
+        $response = Http::patch($this->API_URL."/{$id}",$request); 
         $posts = $response->json();
         return response()->json($posts);       
     }
 
     public function destroy(string $id)
     {
-        $response = Http::delete("{$this->API_URL}/posts/{$id}"); 
+        $response = Http::delete($this->API_URL."/{$id}"); 
         $posts = $response->json();
         return response()->json($posts); 
     }

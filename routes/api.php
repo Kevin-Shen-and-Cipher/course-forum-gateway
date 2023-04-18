b02f7ccc986775666f026f12be7662dd7135e35a
Dragon <C109151163@nkust.edu.tw>
2023-04-18 12:21:51 +0000

    feat:Add tag function

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagsController;
 
 Route::apiResource('/posts', PostController::class);
+Route::apiResource('/tags', TagsController::class);
 
 

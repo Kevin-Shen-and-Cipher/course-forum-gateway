75e693b33f553181a16c4460ab98b1c5eb23c5f5
Dragon <C109151163@nkust.edu.tw>
2023-04-18 11:46:33 +0000

    fix:Url style change

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,47 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class PostController extends Controller 
+class PostController extends Controller
 {
-    private  string $API_URL; 
+    private  string $API_URL;
 
     public function __construct()
     {
-        $this->API_URL = env('APP_COURSE_FORUM_API')."/posts" ;
+        $envAPI  = env('APP_COURSE_FORUM_API');
+        $this->API_URL = "{$envAPI}/posts";
     }
 
     public function index()
     {
-        $response = Http::get($this->API_URL); 
+        $response = Http::get($this->API_URL);
         $posts = $response->json();
         return response()->json($posts);
     }
 
     public function store(Request $request)
     {
-        $data = $request->all(); 
-        $response = Http::post($this->API_URL, $data); 
+        $data = $request->all();
+        $response = Http::post($this->API_URL, $data);
         return $response->json();
     }
 
     public function show(string $id)
     {
-        $response = Http::get($this->API_URL."/{$id}"); 
+        $response = Http::get("{$this->API_URL}/{$id}");
         $posts = $response->json();
         return response()->json($posts);
     }
 
     public function update(Request $request, string $id)
     {
-        $response = Http::patch($this->API_URL."/{$id}",$request); 
+        $response = Http::patch("{$this->API_URL}/{$id}", $request);
         $posts = $response->json();
-        return response()->json($posts);       
+        return response()->json($posts);
     }
 
     public function destroy(string $id)
     {
-        $response = Http::delete($this->API_URL."/{$id}"); 
+        $response = Http::delete("{$this->API_URL}/{$id}");
         $posts = $response->json();
-        return response()->json($posts); 
+        return response()->json($posts);
     }
 }

fab06b3b61e577ea50682d8e8508367abb93fc40
Dragon <C109151163@nkust.edu.tw>
2023-05-09 11:20:49 +0000

    fix:Add APP_COURSE_AUTH_API in env.example

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ use App\Http\Controllers\AuthController;
 Route::apiResource('/posts', PostController::class);
 Route::apiResource('/tags', TagController::class);
 Route::post('/login', [AuthController::class,'login']);
+Route::post('/verify', [AuthController::class,'verify']);
 
 
